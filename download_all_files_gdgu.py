3ac1c424442eadc9e5a56eed50bda7b58494e3c6
Suharsh Tyagi <suharsh96@gmail.com>
2018-05-07 23:12:14 +0530

    Add files via upload

---
+++ download_all_files_gdgu.py
@@ -0,0 +1,13 @@
+from selenium import *
+from selenium import webdriver
+import time
+
+DRIVER = 'C:/chromedriver.exe'
+driver = webdriver.Chrome(DRIVER)
+website = 'https://sites.google.com/site/csedsa201' #enter the gdgu website here
+driver.get(website + '/file-cabinet/') 
+
+continue_link = driver.find_elements_by_link_text('Download')
+for i in range(0,len(continue_link)):
+	continue_link[i].click()
+	time.sleep(5)
